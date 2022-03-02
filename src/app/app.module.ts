d6d0a758ca0ae327bda8576d44d5d494f71a2b1c
Elaine Gonçalves de Oliveira <elaine.a.de.oliveira@avanade.com>
2022-03-02 17:54:35 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { TransactionsTableComponent } from './components/transactions-table/transactions-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SummaryComponent,
+    TransactionsTableComponent
   ],
   imports: [
     BrowserModule,
